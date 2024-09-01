049575ee2e0a26f630d060e9795ddb41be469168
Abd Elrhman Shata <abdelrhmanmohamedshata@gmail.com>
2024-09-02 02:58:46 +0300

    Error Handling
    Add  Error Handling with Express in all app

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({
   path: './config.env'
 });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-  '<db_password>',
+  '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-const DBLocal = process.env.DATABASE_LOCAL
-
 mongoose
-  // .connect(DBLocal, {
   .connect(DB, {
     // useNewUrlParser: true,
-    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false
   })
-  .then(() => console.log('DB connection successful!'))
-  .catch(err => console.log('DB connection error:', err));
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
